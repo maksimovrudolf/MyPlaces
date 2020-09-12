4b1984c5e946c708533637639dad8d9421a712b2
Rudolf Maksimov <maksimovrudolf@gmail.com>
2020-09-12 12:55:23 +0300

    Model New Place Controller

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,87 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Рудольф Максимов on 12.09.2020.
+//  Copyright © 2020 Rudolf Maksimov. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Убираем лишние линии
+        tableView.tableFooterView = UIView()
+    }
+
+    //MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let phohoIcon = #imageLiteral(resourceName: "photo")
+            
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            photo.setValue(phohoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    // Скрываем клавиатуру по нажатию на Done
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
+
+// MARK: Work with image
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
+    }
+}
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,34 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Рудольф Максимов on 12.09.2020.
+//  Copyright © 2020 Rudolf Maksimov. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+}
